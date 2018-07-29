9d2b9cc195f43c98b42a2537db6fda18186c6ec5
Songmu <y.songmu@gmail.com>
2018-07-30 00:12:20 +0900

    TestLoadConfigFiles

--- config_test.go
+++ config_test.go
@@ -1,44 +1,107 @@
 package main
 
 import (
-	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
-func TestGetConfig(t *testing.T) {
-	c := &config{
-		Default: &blogConfig{
-			LocalRoot: "./data",
-		},
-		Blogs: map[string]*blogConfig{
-			"blog.example.com": {
-				RemoteRoot: "blog.example.com",
-				Username:   "xxx",
-				Password:   "yyy",
+func TestLoadConfigFiles(t *testing.T) {
+	orig := homedir.DisableCache
+	homedir.DisableCache = true
+	defer func() {
+		homedir.DisableCache = orig
+	}()
+
+	setup := func(t *testing.T, localConf, globalConf *string) (string, func()) {
+		tempdir, err := ioutil.TempDir("", "blogsync-test")
+		if err != nil {
+			t.Fatal(err)
+		}
+		origHome := os.Getenv("HOME")
+		cleanup := func() {
+			os.RemoveAll(tempdir)
+			os.Setenv("HOME", origHome)
+		}
+
+		if localConf != nil {
+			err := ioutil.WriteFile(
+				filepath.Join(tempdir, "blogsync.yaml"), []byte(*localConf), 0755)
+			if err != nil {
+				cleanup()
+				t.Fatal(err)
+			}
+		}
+
+		if globalConf != nil {
+			globalConfFile := filepath.Join(tempdir, ".config", "blogsync", "config.yaml")
+			err := os.MkdirAll(filepath.Dir(globalConfFile), 0755)
+			if err != nil {
+				cleanup()
+				t.Fatal(err)
+			}
+			err = ioutil.WriteFile(globalConfFile, []byte(*globalConf), 0755)
+			if err != nil {
+				cleanup()
+				t.Fatal(err)
+			}
+		}
+
+		err = os.Setenv("HOME", tempdir)
+		if err != nil {
+			cleanup()
+			t.Fatal(err)
+		}
+		return tempdir, cleanup
+	}
+
+	pstr := func(str string) *string {
+		return &str
+	}
+	testCases := []struct {
+		name       string
+		localConf  *string
+		globalConf *string
+
+		blogKey string
+		expect  blogConfig
+	}{
+		{
+			name:      "simple",
+			localConf: nil,
+			globalConf: pstr(`---
+              default:
+                local_root: ./data
+              blog1.example.com:
+                username: blog1
+              blog2.example.com:
+                local_root: ./blog2`),
+			blogKey: "blog1.example.com",
+			expect: blogConfig{
+				RemoteRoot: "blog1.example.com",
+				LocalRoot:  "./data",
+				Username:   "blog1",
 			},
 		},
 	}
 
-	bc := c.Get("blog.example.com")
-	assert.NotNil(t, bc)
-	assert.Equal(t, "./data", bc.LocalRoot)
-}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			workdir, teardown := setup(t, tc.localConf, tc.globalConf)
+			defer teardown()
+			conf, err := loadConfigFiles(workdir)
+			if err != nil {
+				t.Errorf("error should be nil but: %s", err)
+			}
+			out := conf.Get(tc.blogKey)
 
-func TestLoadConfig(t *testing.T) {
-	r := bytes.NewReader([]byte(
-		`---
-default:
-  local_root: ./data
-blog1.example.com:
-  username: blog1
-blog2.example.com:
-  local_root: ./blog2`,
-	))
-	c, err := loadConfig(r)
-	assert.Nil(t, err)
-	assert.Equal(t, "./data", c.Default.LocalRoot)
-	assert.Equal(t, "blog1", c.Blogs["blog1.example.com"].Username)
-	assert.Equal(t, "blog1.example.com", c.Blogs["blog1.example.com"].RemoteRoot)
+			if !reflect.DeepEqual(*out, tc.expect) {
+				t.Errorf("something went wrong.\n   out: %+v\nexpect: %+v", *out, tc.expect)
+			}
+		})
+	}
 }
