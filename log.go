eb967e425eb523cb5e5be956baa95ea74b9fe086
Songmu <y.songmu@gmail.com>
2023-10-21 23:06:23 +0900

    [incompatible] change log output dest to stderr

--- log.go
+++ log.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/motemen/go-colorine"
 )
 
-var logger = &colorine.Logger{
-	Prefixes: colorine.Prefixes{
-		"http":  colorine.Verbose,
-		"store": colorine.Info,
-		"error": colorine.Error,
-		"":      colorine.Verbose,
-	},
+var logger *colorine.Logger
+
+func init() {
+	logger = &colorine.Logger{
+		Prefixes: colorine.Prefixes{
+			"http":  colorine.Verbose,
+			"store": colorine.Info,
+			"error": colorine.Error,
+			"":      colorine.Verbose,
+		}}
+	logger.SetOutput(os.Stderr)
 }
 
 func logf(prefix, pattern string, args ...interface{}) {
