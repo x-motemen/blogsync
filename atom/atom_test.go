eba08706d393bce31084f42107ba17de374ddf34
Songmu <y.songmu@gmail.com>
2023-10-24 18:12:22 +0900

    drop testify from atom/atom_test.go

--- atom/atom_test.go
+++ atom/atom_test.go
@@ -3,17 +3,22 @@ package atom
 import (
 	"os"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestParse(t *testing.T) {
 	f, err := os.Open("../example/hatenablog.xml")
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 	defer f.Close()
 
 	feed, err := Parse(f)
-	assert.NoError(t, err)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	assert.Equal(t, "2014-11-20 14:48:59 +0000 UTC", feed.Entries[0].Edited.UTC().String())
+	expect := "2014-11-20 14:48:59 +0000 UTC"
+	if g := feed.Entries[0].Edited.UTC().String(); g != expect {
+		t.Errorf("expect: %s, but got: %s", expect, g)
+	}
 }
